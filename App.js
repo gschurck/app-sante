5a85fb5629f4c2da9abe9c2c31b5170fa6ab89cf
Wylhem dorville <62334126+Wylhem@users.noreply.github.com>
2021-03-11 19:32:56 +0100

    adding stylesSheets

--- App.js
+++ App.js
@@ -1,29 +1,21 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
-import Home from "./screens/Home/Home";
-
+import styles from "./style";
 
 class App extends Component {
     render() {
         return (
-            <View style ={styles.container}>
+            <View style={styles.container}>
                <Text>Hello les gens</Text>
             </View>
         );
     }
 }
 
-import {StyleSheet} from "react-native-web";
 
-const styles = StyleSheet.create({
-    container : {
-        width : "100%",
-        height:"100%",
-        flex: 1,
-        justifyContent :"center",
-        alignItems : "center",
-    }
-});
+
+
+
 
 export default App;
 
